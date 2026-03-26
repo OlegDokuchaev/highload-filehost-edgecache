8da3334c6f44007d5386cc3e6c1a36150c469718
Ivan Smirnov <IvanSmirnov2002@yandex.ru>
2026-03-26 20:46:31 +0300

    Revert "Feat/users service auth"

--- users-service/src/users_service/db/init_db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncEngine
-
-from users_service.db.base import Base
-from users_service.db import models  # noqa: F401
-
-
-async def create_tables(engine: AsyncEngine) -> None:
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
