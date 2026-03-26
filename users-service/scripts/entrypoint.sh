cdcc19fbf59af8a435585db52cca44125f7a72e4
AivanSpooky <IvanSmirnov2002@yandex.ru>
2026-03-26 15:48:43 +0300

    feat(users): внедрён Alembic и initial миграция, create_all убран из lifespan

---
+++ users-service/scripts/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Запуск миграций Alembic..."
+alembic upgrade head
+echo "Старт users-service..."
+exec uvicorn "users_service.main:create_app" --factory --host "0.0.0.0" --port "8001"
+
